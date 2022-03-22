770317631cef68c40b174fddd8bf63f38d9d9a93
erika mayoute <e.mayoute44@gmail.com>
2022-03-22 19:02:07 +0100

    Add navbar via yield

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'service', to: 'pages#service'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
