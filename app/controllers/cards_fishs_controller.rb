21a3d8af902cd64a911f5c775f078b92ca2403ef
erika mayoute <e.mayoute44@gmail.com>
2022-03-25 15:09:29 +0100

    Add index for controller CFS

--- app/controllers/cards_fishs_controller.rb
+++ app/controllers/cards_fishs_controller.rb
@@ -2,4 +2,7 @@ class CardsFishsController < ApplicationController
 
   def index
   end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'service', to: 'pages#service'
   get 'dashboard', to: 'pages#dashboard'
   get 'cards_fishs', to: 'cards_fishs#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
