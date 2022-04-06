c66bc95a4b7628350b7706a2b3e9f29dee42a8e1
erika mayoute <e.mayoute44@gmail.com>
2022-04-06 15:08:07 +0200

    Add logout home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'service', to: 'pages#service'
   get 'dashboard', to: 'pages#dashboard'
   get 'cards_fishs', to: 'cards_fishs#index'
+  get 'users/sign_out', to: 'sessions#destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
