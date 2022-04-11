5eaf2d27d92f3457f17d058d143029aad2477375
erika mayoute <e.mayoute44@gmail.com>
2022-04-11 12:15:36 +0200

    Fixed bug logout user

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get 'service', to: 'pages#service'
   get 'dashboard', to: 'pages#dashboard'
   get 'cards_fishs', to: 'cards_fishs#index'
-  get 'users/sign_out', to: 'sessions#destroy'
 
+  devise_scope :user do
+  get 'users/sign_out', to: 'sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
