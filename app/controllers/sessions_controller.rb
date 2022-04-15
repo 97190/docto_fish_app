d7f44f7389ca6b9b0f68b7bdc9d7e04dd8af91a9
erika mayoute <e.mayoute44@gmail.com>
2022-04-15 16:05:49 +0200

    Default logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
-  def destroy
-    session[:current_user_id] = nil
-    flash[:notice] = "You have successfully logged out."
-    redirect_to root_url
-  end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to root_url, notice: "Logged out!"
+end
+
+
+
+  # def destroy
+  #   session[:current_user_id] = nil
+  #   flash[:notice] = "You have successfully logged out."
+  #   redirect_to root_url
+  # end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,21 +3,28 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+#   GET /resource/sign_in
+#   def new
+#     super
+#   end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+#   POST /resource/sign_in
+#   def create
+#     super
+#   end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
+#   DELETE /resource/sign_out
+#   def destroy
+#     session[:current_user_id] = nil
+#     flash[:notice] = "You have successfully logged out."
+#     redirect_to root_url
+#   end
+# end
+# def destroy
+#     session[:current_user_id] = nil
+#     flash[:notice] = "You have successfully logged out."
+#     redirect_to root_url
+#   end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
