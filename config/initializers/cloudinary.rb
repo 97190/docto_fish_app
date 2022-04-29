df3e2587254438ecf0990f03bea1c71fe0ccb22e
erika mayoute <e.mayoute44@gmail.com>
2022-04-29 16:26:40 +0200

    Setup suite cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary .config do |config|
+  config.cloud_name = ' dgtcdn0q2 '
+  config.api_key = ' 469871657879661 '
+  config.api_secret = ' oa20K4_m92clkYEq2H-R174IRfk '
+  config.secure = true
+  config.cdn_subdomain = true
+end
