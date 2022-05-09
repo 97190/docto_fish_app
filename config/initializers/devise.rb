0e3ea22f229c2cadc0b1540e8a645b97c54ef5ff
erika mayoute <e.mayoute44@gmail.com>
2022-05-09 11:50:55 +0200

    Fixed bug devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -237,6 +237,7 @@ Devise.setup do |config|
   # for default behavior) and :restful_authentication_sha1 (then you should set
   # stretches to 10, and copy REST_AUTH_SITE_KEY to pepper).
   #
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
   # Require the `devise-encryptable` gem when using anything other than bcrypt
   # config.encryptor = :sha512
 
